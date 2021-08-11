6e10d05bcfab6c3fbf20f4b4d823a6a6a81e6889
Anatoliy-1986 <Gospodarikov_aa@mail.ru>
2021-08-11 16:44:49 +0300

    eigth commit

--- src/Task.js
+++ src/Task.js
@@ -26,15 +26,18 @@ function Task () {
 	const handleChecked = (currentIndex) => (event) => {
 		list.map((item, index) => index === currentIndex ? {...item, checked:event.target.checked} : item)  
 	}
-
+/* {!isEdit ? <input value={item} onChange={event => setValue(event.target.value)}
+			    onBlur={() => setIsEdit(false)}*/
 
 	return <div className="Todo">
           {list.map((item, index) =>
 		  <label key={index}>
 			 <input type="checkbox" id={index} checked={item.disabled} onChange={handleChecked(index)} />
-			 {!isEdit ? <span onClick={() => setIsEdit(true)}>{item}</span> : 
-			 <input value={item} onChange={event => setValue(event.target.value)}
-			    onBlur={() => setIsEdit(false)}/>}</label>}
+			 {!isEdit ? <span onClick={() => setIsEdit(true)}>{item}</span> : <input id={item.id} value={item} onChange={event => setValue(event.target.value)}
+			    onBlur={() => setIsEdit(false)}/>}
+
+
+			 </label>)}
 			<input onChange={enteryText} value={value} />
 			<button onClick={addText}>Добавить в список</button>	
 	</div>;
